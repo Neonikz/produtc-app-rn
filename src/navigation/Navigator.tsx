3311eef09c28a4ea8caffbdb23af8d646c4f367d
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2023-05-31 10:56:34 -0300

    first commit

---
+++ src/navigation/Navigator.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreen } from '../screens/LoginScreen';
+import { RegisterScreen } from '../screens/RegisterScreen';
+import { ProtectedScreen } from '../screens/ProtectedScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+        </Stack.Navigator>
+    );
+};
